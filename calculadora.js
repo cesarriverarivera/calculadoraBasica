ddc3e13facf94c2795099cba035b06a38cbd8e83
César Rivera <cesarrivera2622@gmail.com>
2023-06-30 12:23:22 -0500

    modificacion de logica de calculo y limpieza de codigo no utilizado

--- calculadora.js
+++ calculadora.js
@@ -2,7 +2,7 @@
 const pantalla = document.querySelector("#screen");
 const pantalla2 = document.querySelector("#screen2");
 const botonNumero = document.querySelectorAll(".num");
-const botonOperador = document.querySelectorAll(".operador");
+const elementos = document.querySelector('.imagen');
 console.log(botonNumero);
 
 let num0 = botonNumero[9].innerHTML;
@@ -39,156 +39,85 @@ const borrarUnitario = document.querySelector("#borrarUnitario");
 
 
 let valor1;
-let valor2="";
 let resultado;
-let operadorClicked = false;
-let suma = false;
-let resta =false;
-let multiplicacion =false;
-let division =false;
+
+
+
 
 
 //CLICK A CADA UNO DE LOS BOTONES PARA QUE SE MUESTREN EN PANTALLA Y ALMACENEN EL VALOR1 Y VALOR 2
 botonPunto.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + punto;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + punto;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 
 boton1.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num1;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num1;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton2.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num2;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num2;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton3.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num3;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num3;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton4.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num4;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num4;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton5.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num5;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num5;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton6.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num6;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num6;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton7.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num7;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num7;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton8.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num8;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num8;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton9.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num9;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num9;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
 
 boton0.addEventListener("click", ()=>{
-    if(operadorClicked ===false){
         pantalla.innerHTML = pantalla.innerHTML + num0;
         valor1 = pantalla.innerHTML;
         console.log({valor1})
-    }else if(operadorClicked === true){
-        pantalla2.innerHTML = pantalla2.innerHTML + num0;
-        valor2 = pantalla2.innerHTML;
-        console.log({valor2})  
-    }
 });
-
-
 
 
 //BOTONES DE OPERACION SUMA, RESTA, MULTIPLICACION Y DIVISION
 botonSuma.addEventListener("click",()=>{
-    operadorClicked =true;
-    suma =true;
+    pantalla.innerHTML = pantalla.innerHTML + "+";
+    valor1 = pantalla.innerHTML;
 })
 
 botonResta.addEventListener("click",()=>{
@@ -196,20 +125,20 @@ botonResta.addEventListener("click",()=>{
         operadorClicked=false;
         pantalla.innerHTML = pantalla.innerHTML + "-";
     }else{
-        operadorClicked =true;
-        resta =true;
+        pantalla.innerHTML = pantalla.innerHTML + "-";
+        valor1 = pantalla.innerHTML;
     }
     
 })
 
 botonMultiplicacion.addEventListener("click",()=>{
-    operadorClicked =true;
-    multiplicacion =true;
+    pantalla.innerHTML = pantalla.innerHTML + "*";
+    valor1 = pantalla.innerHTML;
 })
 
 botonDivision.addEventListener("click",()=>{
-    operadorClicked =true;
-    division =true;
+    pantalla.innerHTML = pantalla.innerHTML + "/";
+    valor1 = pantalla.innerHTML;
 })
 
 
@@ -217,80 +146,24 @@ botonDivision.addEventListener("click",()=>{
 
 //BOTON IGUAL PARA CALCULAR LA OPERACION
 botonResultado.addEventListener("click", ()=>{
-    if(suma===true){
-        pantalla.innerHTML ="";
-        pantalla2.innerHTML ="";
-        resultado = Number((parseFloat(valor1) + parseFloat(valor2)).toFixed(10));
-        console.log(resultado);
-        valor1=resultado;
-        pantalla.innerHTML=valor1;
-        suma =false;
-        operadorClicked=false;
-        pantalla2.innerHTML="";
-    }else if(resta ===true){
-        pantalla.innerHTML ="";
-        pantalla2.innerHTML ="";
-        resultado = Number((parseFloat(valor1) - parseFloat(valor2)).toFixed(10));
-        console.log(resultado);
-        valor1=resultado;
-        pantalla.innerHTML=valor1;
-        resta =false;
-        operadorClicked=false;
-        pantalla2.innerHTML=""; 
-    }else if(multiplicacion ===true){
-        pantalla.innerHTML ="";
-        pantalla2.innerHTML ="";
-        resultado = Number((parseFloat(valor1) * parseFloat(valor2)).toFixed(10));
-        console.log(resultado);
-        valor1=resultado;
-        pantalla.innerHTML=valor1;
-        multiplicacion =false;
-        operadorClicked=false;
-        pantalla2.innerHTML="";
-    }else if(division ===true){
-        pantalla.innerHTML ="";
-        pantalla2.innerHTML ="";
-        resultado = Number((parseFloat(valor1) / parseFloat(valor2)).toFixed(10));
-        console.log(resultado);
-        valor1=resultado;
-        pantalla.innerHTML=valor1;
-        division =false;
-        operadorClicked=false;
-        pantalla2.innerHTML="";
-    }
+    pantalla2.innerHTML = `= ${Number(parseFloat(eval(valor1)).toFixed(10))}`;
+    elementos.style.animation = 'aparecer 3s infinite'; 
 })
 
 //BOTON BORRAR TODO
 botonBorrar.addEventListener("click", ()=>{
     pantalla.innerHTML ="";
     pantalla2.innerHTML ="";
-    valor1="";
-    valor2="";
-    suma=false;
-    resta=false;
-    multiplicacion=false;
-    division=false;
-    operadorClicked=false;
+    valor1=""; 
+    elementos.style.animation = '';   
 })
 
 
 //BOTON BORRAR UNO POR UNO
 borrarUnitario.addEventListener("click", ()=>{
-    if(valor2 ==""){
-        valor1 = valor1.slice(0, -1);
-        pantalla.innerHTML =valor1;
-    }else if(valor1 !== ""){
-        valor2 = valor2.slice(0, -1);
-        pantalla2.innerHTML = valor2; 
-        if(valor2 == ""){
-            suma=false;
-            resta=false;
-            multiplicacion=false;
-            division=false;
-            operadorClicked=false;  
-        }
-    }
-    
+    valor1 = valor1.slice(0, -1);
+    console.log(valor1);
+    pantalla.innerHTML =valor1;
+    elementos.style.animation = '';
 })
-
 
